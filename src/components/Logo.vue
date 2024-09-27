3abb49d6b1b29d1df7bc47c62aeee9c3096cbe52
chocycat <chocycat@catboy.email>
2024-09-27 23:07:24 +0200

    feat: Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    class="aspect-square h-auto"
+    width="200"
+    height="200"
+    viewBox="0 0 200 200"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      class="fill-theme-700"
+      d="M100 95.664C100 42.8302 142.83 0 195.664 0V0C198.059 0 200 1.94131 200 4.33604V184C200 192.837 192.837 200 184 200H100V95.664Z" />
+    <path
+      class="fill-theme-600"
+      d="M-1.52588e-05 195.664C-1.52588e-05 142.83 42.8302 100 95.6639 100H200V184C200 192.837 192.837 200 184 200H4.33603C1.9413 200 -1.52588e-05 198.059 -1.52588e-05 195.664V195.664Z" />
+    <circle
+      class="fill-accent-500"
+      cx="100"
+      cy="100"
+      r="50"
+      transform="rotate(-90 100 100)" />
+  </svg>
+</template>
