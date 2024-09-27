f043bf406b0dd727c36ee95bd5f5f9d93cea7a7e
chocycat <chocycat@catboy.email>
2024-09-27 17:41:41 +0200

    feat: Icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+defineProps<{ icon: string }>();
+</script>
+
+<template>
+  <span>{{ icon }}</span>
+</template>
+
+<style lang="scss" scoped>
+span {
+  @apply select-none;
+
+  --filled: 1;
+  --weight: 400;
+
+  display: inline-block;
+  direction: ltr;
+  font-family: 'Material Symbols Rounded', sans-serif;
+  font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+  font-variation-settings:
+    'FILL' var(--filled),
+    'wght' var(--weight),
+    'GRAD' 0,
+    'opsz' 24;
+  letter-spacing: normal;
+  line-height: 1;
+  text-transform: none;
+  vertical-align: middle;
+  white-space: nowrap;
+  word-wrap: normal;
+}
+</style>
