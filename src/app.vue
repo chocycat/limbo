5268401902c2c47e870832b09d8ec8ad5d724eaf
chocycat <chocycat@catboy.email>
2024-09-27 18:39:38 +0200

    feat: debug route for testing components

--- src/app.vue
+++ src/app.vue
@@ -5,6 +5,6 @@ useTheme();
 
 <template>
   <main>
-    <h1 class="mx-auto my-auto text-4xl font-bold">Hello, world!</h1>
+    <NuxtPage />
   </main>
 </template>
---
+++ src/pages/_components.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-4 p-8">
+    <h1 class="text-3xl font-bold">Hello, world!</h1>
+
+    <p class="text-theme-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo vero
+      earum error neque laudantium voluptate debitis? Natus magni sapiente, sit
+      quis officiis velit. Fugit, vel maiores odio quis earum dolore?
+    </p>
+
+    <p class="text-sm text-theme-500">
+      *Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+    </p>
+
+    <div class="flex items-center gap-2">
+      <Button text="Small Button" size="small" />
+      <Button text="Accent Button" variant="accent" size="small" />
+    </div>
+
+    <div class="flex items-center gap-2">
+      <Button text="Regular Button" />
+      <Button text="Accent Button" icon="favorite" variant="accent" />
+      <Button text="Warning Button" variant="warning" />
+      <Button text="Error Button" variant="error" />
+      <Button text="Transparent Button" variant="transparent" />
+    </div>
+
+    <div class="flex items-center gap-2">
+      <Button text="Large Button" size="large" />
+      <Button text="Accent Button" variant="accent" size="large" />
+    </div>
+  </div>
+</template>
