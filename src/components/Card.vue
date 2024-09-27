90d0c9a01cd70d6a3b3ced7a3df1b1f6afbac990
chocycat <chocycat@catboy.email>
2024-09-27 19:33:53 +0200

    feat: Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const emit = defineEmits<{ (e: 'click', event: MouseEvent): void }>();
+defineProps<{ clickable?: boolean }>();
+
+const slots = useSlots();
+</script>
+
+<template>
+  <div
+    class="ease-standard flex flex-col rounded-2xl bg-theme-900 transition"
+    :class="{
+      'group cursor-pointer select-none hover:bg-theme-800': clickable,
+    }"
+    @click="emit('click', $event)">
+    <div class="flex flex-col gap-2 p-4">
+      <slot />
+    </div>
+
+    <div
+      v-if="slots.footer"
+      class="ease-standard flex items-center gap-2 border-t border-theme-800 p-4 transition-colors"
+      :class="{ 'group-hover:border-theme-700': clickable }">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
--- src/pages/_components.vue
+++ src/pages/_components.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-4 p-8">
-    <h1 class="text-3xl font-bold">Hello, world!</h1>
+    <h1 class="text-3xl font-bold">Components</h1>
 
     <p class="text-theme-300">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo vero
@@ -11,6 +11,8 @@
     <p class="text-sm text-theme-500">
       *Lorem ipsum dolor, sit amet consectetur adipisicing elit.
     </p>
+
+    <h2 class="text-lg font-bold">Buttons</h2>
 
     <div class="flex items-center gap-2">
       <Button text="Small Button" size="small" />
@@ -30,11 +32,42 @@
       <Button text="Accent Button" variant="accent" size="large" />
     </div>
 
-    <div class="flex w-[300px] flex-col gap-4">
+    <h2 class="text-lg font-bold">Text Inputs</h2>
+
+    <div class="grid w-[600px] grid-cols-2 gap-4">
       <InputText id="1" />
       <InputText id="2" placeholder="With Placeholder" />
       <InputText id="3" label="With Label" />
       <InputText id="4" label="With Icon" icon="language" />
+    </div>
+
+    <h2 class="text-lg font-bold">Cards</h2>
+
+    <div class="grid w-full grid-cols-2 gap-4">
+      <Card :clickable="true">
+        <h3 class="text-lg font-bold">Card #1</h3>
+        <p class="text-theme-300">
+          <b class="text-theme-50">Hover over me!</b> Lorem ipsum, dolor sit
+          amet consectetur adipisicing elit. Animi obcaecati delectus,
+          similique, sit laboriosam et illo dolores quis, vero ducimus nulla
+          tempora nesciunt distinctio velit corporis aperiam.
+        </p>
+      </Card>
+      <Card>
+        <h3 class="text-lg font-bold">Card #2</h3>
+        <p class="text-theme-300">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi
+          obcaecati delectus, similique, sit laboriosam, vero ducimus nulla
+          tempora nesciunt.
+        </p>
+
+        <template #footer>
+          <div class="ml-auto flex items-center gap-2">
+            <Button text="Cancel" size="small" variant="transparent" />
+            <Button text="OK" size="small" variant="accent" />
+          </div>
+        </template>
+      </Card>
     </div>
   </div>
 </template>
