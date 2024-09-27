6ce54a1ee13218f6baebb7c3776beb2d8770c7fe
chocycat <chocycat@catboy.email>
2024-09-28 01:29:18 +0200

    feat: loading state on Buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,34 +1,58 @@
 <script lang="ts" setup>
 const emit = defineEmits<{ (e: 'click', event: MouseEvent): void }>();
 
-const { text, icon } = withDefaults(
+const props = withDefaults(
   defineProps<{
     variant?: 'transparent' | 'theme' | 'accent' | 'error' | 'warning';
     size?: 'small' | 'base' | 'large';
     text?: string;
     icon?: string | { leading?: string; trailing?: string };
+    loading?: boolean;
+    disabled?: boolean;
   }>(),
   { variant: 'theme', size: 'base' }
 );
 
-const simpleMode = computed(() => !!icon || !!text);
+const { loading, icon, text, disabled } = toRefs(props);
+
+const simpleMode = computed(
+  () => !!loading.value || !!icon.value || !!text.value
+);
 const leadingIcon = computed(() =>
-  typeof icon === 'string' ? icon : icon?.leading
+  typeof icon.value === 'string' ? icon.value : icon.value?.leading
 );
 const trailingIcon = computed(() =>
-  typeof icon === 'object' ? icon?.trailing : null
+  typeof icon.value === 'object' ? icon.value?.trailing : null
 );
+
+const spinnerClass = computed(() => {
+  // TODO: refactor this, this is quite unconventional
+  if (
+    ['accent', 'error', 'warning'].includes(props.variant) &&
+    !disabled.value
+  ) {
+    return 'stroke-theme-950';
+  } else {
+    return 'stroke-theme-50';
+  }
+});
 </script>
 
 <template>
   <button
     :data-size="size"
     :data-variant="variant"
+    :disabled="disabled"
     @click="emit('click', $event)">
     <template v-if="simpleMode">
-      <Icon v-if="leadingIcon" class="-ml-0.5" :icon="leadingIcon" />
-      <span v-if="text">{{ text }}</span>
-      <Icon v-if="trailingIcon" class="-mr-0.5" :icon="trailingIcon" />
+      <template v-if="loading">
+        <Spinner :spinner-class="spinnerClass" class="h-6 w-6" />
+      </template>
+      <template v-else>
+        <Icon v-if="leadingIcon" class="-ml-0.5" :icon="leadingIcon" />
+        <span v-if="text">{{ text }}</span>
+        <Icon v-if="trailingIcon" class="-mr-0.5" :icon="trailingIcon" />
+      </template>
     </template>
     <slot v-else />
   </button>
