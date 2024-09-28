34f20232c07d1be0610c40a4531e9d693e415521
chocycat <chocycat@catboy.email>
2024-09-28 11:35:01 +0200

    feat: generic CloseButton

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,6 @@
+<template>
+  <button
+    class="flex aspect-square h-6 w-6 items-center justify-center rounded-full bg-transparent text-theme-300 transition ease-standard hover:bg-theme-600 hover:text-theme-50 active:scale-95">
+    <Icon icon="close" />
+  </button>
+</template>
