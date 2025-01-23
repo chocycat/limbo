0e1c8c8dc756d2a3290d4130b4353828d25eb834
chocycat <chocycat@catboy.email>
2025-01-23 21:15:40 +0200

    fix: login layout

--- src/pages/auth.vue
+++ src/pages/auth.vue
@@ -7,8 +7,7 @@ definePageMeta({
 </script>
 
 <template>
-  <BaseRoute
-    class="relative max-h-screen flex-1 items-center justify-end sm:justify-center">
+  <BaseRoute class="relative max-h-screen flex-1 items-center justify-end sm:justify-center">
     <div
       class="z-10 mb-2 flex w-full items-center justify-start gap-4 self-start px-4 pt-6 sm:-mt-12 sm:max-w-[450px] sm:self-center sm:px-0 sm:pt-0">
       <Logo class="w-12" />
@@ -17,12 +16,10 @@ definePageMeta({
     </div>
 
     <Card
-      class="sm:flex-0 relative z-10 max-h-full min-h-0 w-full flex-1 overflow-auto rounded-b-none p-4 sm:h-fit sm:max-h-fit sm:max-w-[450px] sm:rounded-b-2xl">
+      class="sm:flex-grow-0 flex-1 relative z-10 max-h-full min-h-0 w-full overflow-auto rounded-b-none p-4 sm:h-fit sm:max-h-fit sm:max-w-[450px] sm:rounded-b-2xl">
       <NuxtPage />
     </Card>
 
-    <AuthBackground
-      class="z-0"
-      :dot-color="cssColor('hsla(var(--twc-theme-900) / 40%)')" />
+    <AuthBackground class="z-0" :dot-color="cssColor('hsla(var(--twc-theme-900) / 40%)')" />
   </BaseRoute>
 </template>
