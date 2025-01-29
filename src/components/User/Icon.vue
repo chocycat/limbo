9b86623e98835d3df76961019e2f52d66f807f8a
chocycat <chocycat@catboy.to>
2025-01-29 19:12:19 +0100

    feat: allow `UserIcon` to be an actual Icon

--- src/components/User/Icon.vue
+++ src/components/User/Icon.vue
@@ -1,27 +1,21 @@
 <script lang="ts" setup>
 import { RoomMember } from 'matrix-js-sdk';
 
-const { client } = storeToRefs(useMatrix());
-
-const { user, size } = withDefaults(
-  defineProps<{ user?: RoomMember; size?: number }>(),
+const props = withDefaults(
+  defineProps<{ user?: RoomMember; size?: number; icon?: string }>(),
   { user: undefined, size: 24 }
 );
+const { user, size } = toRefs(props);
 
-const avatarUrl = computed(() => {
-  const url = user?.getMxcAvatarUrl();
-  if (!client.value || !url) return null;
-
-  const s = Math.floor(size * window.devicePixelRatio);
-  return client.value.mxcUrlToHttp(url, s, s, 'crop', undefined, false, false);
-});
+const { getAvatar } = useMatrixUser();
+const avatarUrl = computed(() => getAvatar(user.value, size.value));
 </script>
 
 <!-- TODO: remove pengui.png after I make some default avatars -->
 
 <template>
   <div
-    class="overflow-hidden rounded-full object-cover"
+    class="userIcon overflow-hidden rounded-full object-cover"
     :style="{
       minWidth: size + 'px',
       minHeight: size + 'px',
@@ -29,8 +23,20 @@ const avatarUrl = computed(() => {
       maxHeight: size + 'px',
     }">
     <img
+      v-if="!icon"
       class="h-full w-full"
       :src="avatarUrl || '/img/pengui.png'"
       crossorigin="anonymous" />
+    <div
+      v-else
+      class="flex items-center justify-center bg-theme-600"
+      :style="{
+        minWidth: size + 'px',
+        minHeight: size + 'px',
+        maxWidth: size + 'px',
+        maxHeight: size + 'px',
+      }">
+      <Icon class="text-xl text-accent-500" :icon="icon" />
+    </div>
   </div>
 </template>
